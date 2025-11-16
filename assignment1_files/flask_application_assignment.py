71df6cc724e188a53af5f7aeab9ddec191d9b391
hiwaiiii <hiwamoradi08@gmail.com>
2025-11-16 16:28:48 +0100

    edited flask_application_assignment for add_movie_incomplete.html

--- assignment1_files/flask_application_assignment.py
+++ assignment1_files/flask_application_assignment.py
@@ -38,27 +38,42 @@ with app.app_context():
 
 @app.route('/', methods=['GET'])
 def index():
-    movies = Movie.query.all()  # Get all movies from the database
+    movies = Movie.query.all()
     return render_template('index_incomplete.html', movies=movies)
 
 @app.route('/add_movie', methods=['GET', 'POST'])
 def add_movie():
     if request.method == 'POST':
-        # Get items from form
         title = request.form['title']
         year = request.form['year']
         number_of_oscars = request.form['number_of_oscars']
-
-        # Create a new movie entry
         new_movie = Movie(title=title, year=year, number_of_oscars=number_of_oscars)
-
-        # Add to the session and commit to the database
         db.session.add(new_movie)
         db.session.commit()
-
         return redirect(url_for('index'))
 
-    return render_template('add_movie.html')
+    return render_template('add_movie_incomplete.html')
+
+@app.route('/edit_movie/<int:id>', methods=['GET', 'POST'])
+def edit_movie(id):
+    movie = Movie.query.get_or_404(id)
+
+    if request.method == 'POST':
+        movie.title = request.form['title']
+        movie.year = request.form['year']
+        movie.number_of_oscars = request.form['number_of_oscars']
+
+        db.session.commit()
+        return redirect(url_for('index'))
+
+    return render_template('edit_movie.html', movie=movie)
+
+@app.route('/delete_movie/<int:id>')
+def delete_movie(id):
+    movie = Movie.query.get_or_404(id)
+    db.session.delete(movie)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
