7f6f4f001a70becb3f79b26e98cbd94b1cb7acb9
hiwaiiii <hiwamoradi08@gmail.com>
2025-11-23 21:43:24 +0100

    edited to make 404.html work

--- app/routes.py
+++ app/routes.py
@@ -81,3 +81,7 @@ def register_routes(app):
         db.session.delete(movie)
         db.session.commit()
         return redirect(url_for('index'))
+
+    @app.errorhandler(404)
+    def not_found_error(error):
+        return render_template('404.html'), 404
